fa18702724c64baf55f80d58481c631108249711
JacobKaylor <jkaylor20@yahoo.com>
2018-03-11 16:42:21 -0400

    adding a carosel

--- src/js/app.js
+++ src/js/app.js
@@ -35,3 +35,18 @@ if(document.getElementById('expand2').style.display == 'none'){
   document.getElementById('expand2').style.display = 'none';
 }
 }
+let slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+function showDivs(n) {
+    let i;
+    const x = document.getElementsByClassName("jacob-photo");
+    if (n > x.length) {slideIndex = 1} 
+        if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
